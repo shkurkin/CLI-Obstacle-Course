d7874935612e4e7d6c87d5921092c5db26bf78e1
eli shkurkin <eshkurkin@gmail.com>
2013-11-19 22:26:51 -0800

    shkurkin

---
+++ config/environments/backup.rb
@@ -0,0 +1,7 @@
+#!/user.bin/env ruby
+puts ""
+puts ""
+puts "your secret instructions are:"
+puts "put all your previous commands into a text file and email upload it to a gist"
+puts ""
+puts "hint: how can you see all your previous commands easily?"
